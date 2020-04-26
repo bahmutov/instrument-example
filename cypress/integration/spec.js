ae5af48962971db45b50c0e41c6462c4cfd8a413
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-26 09:08:23 -0400

    instrumented and code coverage reports

---
+++ cypress/integration/spec.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+describe('Example app', () => {
+  it('loads', () => {
+    cy.visit('/') // goes to the base url set in "cypress.json"
+    cy.contains('Learn React').should('be.visible')
+  })
+})
