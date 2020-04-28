b950f61474cd0fbfa5bfb1facde5925c30f47cce
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-28 16:47:17 -0400

    playing with eslint

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,5 +3,6 @@ describe('Example app', () => {
   it('loads', () => {
     cy.visit('/') // goes to the base url set in "cypress.json"
     cy.contains('Learn React').should('be.visible')
+    cy.wait(1000)
   })
 })
